3e270ba79e91de6074a69c0f30e85d62976512aa
Harshit Kumar <harshit24164048@akgec.ac.in>
2025-04-20 11:49:29 +0530

    Create 09Area_triangle.py

---
+++ 09Area_triangle.py
@@ -0,0 +1,6 @@
+#Program 3 : Calculate area of triangle
+
+base=int(input("Enter the value of base"))
+height=int(input("Enter the value of height"))
+area=0.5*base*height
+print("The area of triangle is ", area)
