11924c9d9f76ae79acd97314cd9b11a14e3ee412
Harshit Kumar <harshit24164048@akgec.ac.in>
2025-04-22 21:17:29 +0530

    Create11Random_number.py

---
+++ 11Random_number..py
@@ -0,0 +1,6 @@
+#Program  : To generate random number
+
+import random
+a=random.randint(1,10)
+print("The random number between 1 to 10 is : ",a)
+
--- 11Random_number.c
+++
@@ -1,6 +0,0 @@
-# #Program  : To generate random number
-
-# import random
-# a=random.randint(1,10)
-# print("The random number between 1 to 10 is : ",a)
-
