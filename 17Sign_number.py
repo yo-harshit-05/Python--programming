69e654248929fb39d55ea71a43ff9c7182f29e32
Harshit Kumar <harshit24164048@akgec.ac.in>
2025-04-23 23:06:12 +0530

    Create 17Sign_number.py

---
+++ 17Sign_number.py
@@ -0,0 +1,8 @@
+# Program 11 : To check weather a given number is positive, negative or zero
+num=int(input("Enter any number\n"))
+if num>0:
+    print("The given number is positive")
+elif num<0:
+    print("The given number is negative")
+else:
+    print("The given number is zero")
