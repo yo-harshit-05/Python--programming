62b335ce8ebcc8c05a0ea74c8f414e137a7a1598
Harshit Kumar <harshit24164048@akgec.ac.in>
2025-04-23 23:04:51 +0530

    Create 16Swap_without_temp.py

---
+++ 16Swap_without_temp.py
@@ -0,0 +1,7 @@
+# Program 10 : Swap without using temp variable
+
+a=int(input("Enter values for a\n"))
+b=int(input("Enter values for b\n"))
+print(f"Value of a and b before swapping is\na={a},b={b}")
+a,b=b,a
+print(f"Value of a and b after swapping is\na={a},b={b}")
