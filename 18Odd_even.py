19f61cd4a260b285f95c0ac0aaaadc52ac135b9c
Harshit Kumar <harshit24164048@akgec.ac.in>
2025-04-25 22:09:27 +0530

    Create 18Odd_even.py

---
+++ 18Odd_even.py
@@ -0,0 +1,6 @@
+# Program 12 : To check a number is odd or even 
+num=int(input("Enter any number\n"))
+if num%2==0:
+    print("The given number is even")
+else :
+    print("The given number is odd")
